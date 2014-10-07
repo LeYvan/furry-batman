9ffdd296ac47b04f12329c0d471a229926ec6ff9
Yvan Dumont <yvan@leyvan.com>
2014-10-07 19:55:57 -0400

    Ajout classe ListeBornes qui liste tout les bornes. Le script zap-bornes.php retourne un objet JSON contenant toute les bornes.

--- include/bd.php
+++ include/bd.php
@@ -1,27 +1,34 @@
 <?php
+
 require "config.php";
 
-class wfBD
+class ListeBornes 
 {
-	public $connexion;
+	public $objConnexion;
+	public $sql_listerBornes = "SELECT * FROM bornes";
 
-	/*
+	public $bornes;
+
+	/* ===========================================================================
 		Méthode: connexion
 		Description: Établis une connexion à la BD, sauf si il en existe déjà une.
 	*/
 	public function connexion()
 	{
-		if ($connexion == null)
+
+		global $dbHote, $dbNom, $dbUtilisateur, $dbMotPasse;
+
+		if ($this->objConnexion == null)
 		{
 			try 
 			{
-				$connexion = new PDO("mysql:host=$dbHote; dbname=$dbNom", 
+				$this->objConnexion = new PDO("mysql:host=$dbHote; dbname=$dbNom", 
 									 $dbUtilisateur, 
 									 $dbMotPasse, 
 									 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
-				$connexion->setAttribute(PDO::ATTR_ERRMODE, 
-										 PDO::ERRMODE_EXCEPTION);
+				$this->objConnexion->setAttribute(PDO::ATTR_ERRMODE, 
+							    PDO::ERRMODE_EXCEPTION);
 
 			} 
 			catch (PDOException $e) 
@@ -34,20 +41,38 @@ class wfBD
 
 	/*
 		Méthode: connexion
-		Description: Lance une requête sur la BD et retourne le résultat.
+		Description: Récupéré une liste de bornes de la BD.
 	*/
-	public function query($strSQL, $params)
+	public function __construct()
 	{
+		$this->connexion();
+		$this->bornes = array();
+
 		try 
 		{
-			$req = $connexion->prepare($strSQL);
-			$req->execute($params);
-			$prepReqProf->setFetchMode(PDO::FETCH_OBJ);
+			$req = $this->objConnexion->prepare($this->sql_listerBornes);
+			$req->execute();
+			$req->setFetchMode(PDO::FETCH_OBJ);
+
+			while ($borne = $req->fetch())
+			{
+				$this->bornes[] = $borne;
+			}
 		} 
 		catch (PDOException $e) {
 			throw new Exception("Requête à la BD impossible: " . $e->getMessage());
 		}
 	}
 	//=========================================================================
+
+	/*
+		Méthode: getAllBornes
+	*/
+	public function getAllBornes()
+	{
+		return $this->bornes;
+	}
+	//=========================================================================
+
 }
 ?>
