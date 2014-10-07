7c27f037843b66f4271c43030c60dfbca3907891
Yvan Dumont <yvan@leyvan.com>
2014-10-07 18:51:45 -0400

    Ajout des bornes dans la BD

--- zap-bornes-maj.php
+++ zap-bornes-maj.php
@@ -1,4 +1,6 @@
 <?php
+	error_reporting(0);
+	
 	require("include/config.php");
 
 	print("Récupération de la dernière version du fichier de la ville...\r\n");
@@ -14,14 +16,13 @@
 
 	$kmlBornes = $xpath->query("//kml:Folder[@id='kml_ft_WIFI']/kml:Placemark");
 
-	print("Nombre de bornes trouvées: " . $kmlBornes->length . "\r\n");
+	print("Nombre de bornes trouvées: " . $kmlBornes->length . ".\r\n");
 
 	$listeBornes = array();
 
-	$i = 0;
-	foreach ($kmlBornes as $kmlBorne)
+	for ($i = 0; $i < $kmlBornes->length; $i++)
 	{
-		$i++;
+		$kmlBorne = $kmlBornes->item($i+1);
 
 		$objBorne = new stdClass();
 
@@ -29,7 +30,7 @@
 
 		$objBorne->batiment = $xpath->query("kml:ExtendedData/kml:SchemaData/kml:SimpleData[@name='NOM_BATI']",$kmlBorne)->item(0)->nodeValue;
 		$objBorne->rue =      $xpath->query("kml:ExtendedData/kml:SchemaData/kml:SimpleData[@name='RUE']",$kmlBorne)->item(0)->nodeValue;
-		$objBorne->no_civ =   $xpath->query("kml:ExtendedData/kml:SchemaData/kml:SimpleData[@name='NO_CIV']",$kmlBorne)->item(0)->nodeValue;
+		$objBorne->noCivic =  $xpath->query("kml:ExtendedData/kml:SchemaData/kml:SimpleData[@name='NO_CIV']",$kmlBorne)->item(0)->nodeValue;
 		$objBorne->arrond =   $xpath->query("kml:ExtendedData/kml:SchemaData/kml:SimpleData[@name='ARROND']",$kmlBorne)->item(0)->nodeValue;
 
 		$objBorne->pos_raw = $xpath->query("kml:Point/kml:coordinates", $kmlBorne)->item(0)->nodeValue;
@@ -40,7 +41,8 @@
 
 			$objBorne->pos->x = $objBorne->pos_arr[0];
 			$objBorne->pos->y = $objBorne->pos_arr[1];
-			$objBorne->pos->z = $objBorne->pos_arr[2];
+
+			unset($objBorne->pos_raw);
 
 			$listeBornes[$i] = $objBorne;
 		}
@@ -51,17 +53,66 @@
 		die ("Rien à importer.");
 	}
 
-	$strSQL = "DELETE FROM bornes;";
+	print("Nombre de bornes valides: " . count($listeBornes) . ".\r\n" .
+	      "Supression des anciennes bornes...");
 
-	$bd = new PDO("mysql:host=$dbHote; dbname=$dbNom", 
-					 $dbUtilisateur, 
-					 $dbMotPasse, 
-					 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+	try 
+	{
+		$strSQL = "DELETE FROM bornes;";
 
-	$bd->setAttribute(PDO::ATTR_ERRMODE, 
-						PDO::ERRMODE_EXCEPTION);
+		$bd = new PDO("mysql:host=$dbHote; dbname=$dbNom", 
+				$dbUtilisateur, 
+				$dbMotPasse, 
+				array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
-	$bd->exec($strSQL);
+		$bd->setAttribute(PDO::ATTR_ERRMODE, 
+				PDO::ERRMODE_EXCEPTION);
 
-		
+		$bd->exec($strSQL);
+
+		print("Ok!\r\n");
+
+	} catch (PDOException $pdo_e) 
+	{
+		print ("\r\nErreur lors de la tentative de DELETE sur la table 'bornes'.\r\nTerminaison du script d'importation...\r\n");
+		exit();
+	}
+
+	$strSQL = "INSERT INTO bornes (arrond, batiment, rue, noCivic, nom, coord_x, coord_y)\r\n" . 
+		  "VALUES \r\n";
+
+	foreach ($listeBornes as $index => $borne)
+	{
+		// Effectue insertion multiple.
+		// Requête préparées non efficace dans ce scénario.
+		$borne->nom = $bd->quote($borne->nom);
+		$borne->batiment = $bd->quote($borne->batiment);
+		$borne->rue = $bd->quote($borne->rue);
+		$borne->noCivic = $bd->quote($borne->noCivic);
+		$borne->arrond = $bd->quote($borne->arrond);
+
+		$strSQL = $strSQL . "({$borne->arrond},{$borne->batiment},{$borne->rue},{$borne->noCivic},{$borne->nom},{$borne->pos->x},{$borne->pos->y})";
+
+		if ($index < count($listeBornes))
+		{
+			$strSQL = $strSQL . ", \r\n";
+		}
+		else
+		{
+			$strSQL = $strSQL . "\r\n;";
+		}
+	}
+
+	try 
+	{
+		$resulta = $bd->exec($strSQL);
+	}
+	catch (PDOException $pdo_e)
+	{
+		print ("Erreur lors de la tentative d'insertion dans la table 'bornes'.\r\nTerminaison du script d'importation...\r\n");
+		exit();
+
+	}
+
+	print("La mise à jour de la base de donnée des bornes ZAP s'est effectuée correctement!\r\n");
 ?>
